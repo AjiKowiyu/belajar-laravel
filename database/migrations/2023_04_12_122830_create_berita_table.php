a6c9fc9097771e52ea8b39ce2e7f6410201dc677
DESKTOP-ST2H7RM\lenovo <ajikowiyu@gmail.com>
2023-04-12 19:45:46 +0700

    sesi 9: migration (table berita)

---
+++ database/migrations/2023_04_12_122830_create_berita_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeritaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('berita', function (Blueprint $table) {
+            $table->id();
+            $table->string('judul');
+            $table->text('isi');
+            $table->integer('kategori_id');
+            $table->string('user_id');
+            $table->string('status', 7);
+            $table->dateTime('tanggal_create');
+            $table->dateTime('tanggal_update');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('berita');
+    }
+}
